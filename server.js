8edc6f4557ceb54b327d5dd3ea59b5829627bfdd
Joseph Sekwa <joseph.sekwa@azampay.co.tz>
2026-05-04 19:36:03 +0300

    Fix: use mysql2/promise for async/await support

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const crypto  = require('crypto');
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const app       = express();
 const PORT      = process.env.PORT || 3000;
